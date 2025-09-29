0dfd2ec0b132529a0f6d4fe6fd7322cbb294996d
Ronmi Ren <ronmi.ren@gmail.com>
2025-09-30 02:12:21 +0800

    add a fixed sized layout

---
+++ lg/fixed.go
@@ -0,0 +1,175 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package lg
+
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/raohwork/huninn/tapioca"
+)
+
+type fixedInfo struct {
+	size int
+	tea.Model
+}
+
+// FixedLayout is a layout that reserves a fixed amount of space for one component
+// and gives the rest of the space to the other component.
+type FixedLayout struct {
+	reserve    int
+	components [2]fixedInfo
+
+	horizontal bool
+	end        bool
+}
+
+// FixedLeftLayout reserves space on the left side of the layout.
+func FixedLeftLayout(reserve int, left, right tea.Model) *FixedLayout {
+	return newFixed(true, false, reserve, left, right)
+}
+
+// FixedRightLayout reserves space on the right side of the layout.
+func FixedRightLayout(reserve int, left, right tea.Model) *FixedLayout {
+	return newFixed(true, true, reserve, left, right)
+}
+
+// FixedTopLayout reserves space on the top side of the layout.
+func FixedTopLayout(reserve int, top, bottom tea.Model) *FixedLayout {
+	return newFixed(false, false, reserve, top, bottom)
+}
+
+// FixedBottomLayout reserves space on the bottom side of the layout.
+func FixedBottomLayout(reserve int, top, bottom tea.Model) *FixedLayout {
+	return newFixed(false, true, reserve, top, bottom)
+}
+
+func newFixed(horizontal, end bool, reserve int, components ...tea.Model) *FixedLayout {
+	if len(components) != 2 {
+		panic("fixed can only contain up to 2 components")
+	}
+	return &FixedLayout{
+		reserve: reserve,
+		components: [2]fixedInfo{
+			{size: 0, Model: components[0]},
+			{size: 0, Model: components[1]},
+		},
+		horizontal: horizontal,
+		end:        end,
+	}
+}
+
+func (f *FixedLayout) Init() tea.Cmd {
+	cmds := make([]tea.Cmd, 0, 2)
+	for _, c := range f.components {
+		cmd := c.Init()
+		if cmd != nil {
+			cmds = append(cmds, cmd)
+		}
+	}
+	if len(cmds) == 0 {
+		return nil
+	}
+	return tea.Batch(cmds...)
+}
+
+func (f *FixedLayout) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		cmds = append(cmds, f.handleResize(msg.Width, msg.Height)...)
+	case tapioca.ResizeMsg:
+		cmds = append(cmds, f.handleResize(msg.Width, msg.Height)...)
+	default:
+		for i := range f.components {
+			m, cmd := f.components[i].Update(msg)
+			if cmd != nil {
+				cmds = append(cmds, cmd)
+			}
+			f.components[i].Model = m
+		}
+	}
+	if len(cmds) == 0 {
+		return f, nil
+	}
+	return f, tea.Batch(cmds...)
+}
+
+func (f *FixedLayout) handleResize(w, h int) (ret []tea.Cmd) {
+	newSize := h
+	if f.horizontal {
+		newSize = w
+	}
+
+	newMsg := func(s int) tapioca.ResizeMsg {
+		if f.horizontal {
+			return tapioca.ResizeMsg{Width: s, Height: h}
+		}
+		return tapioca.ResizeMsg{Width: w, Height: s}
+	}
+
+	reserveAt := 0
+	if f.end {
+		reserveAt = 1
+	}
+	if f.components[reserveAt].size != f.reserve {
+		f.components[reserveAt].size = f.reserve
+		m, cmd := f.components[reserveAt].Update(newMsg(f.reserve))
+		if cmd != nil {
+			ret = append(ret, cmd)
+		}
+		f.components[reserveAt].Model = m
+	}
+
+	rest := max(newSize-f.reserve, 0)
+
+	f.components[1-reserveAt].size = rest
+	if rest > 0 {
+		m, cmd := f.components[1-reserveAt].Update(newMsg(rest))
+		if cmd != nil {
+			ret = append(ret, cmd)
+		}
+		f.components[1-reserveAt].Model = m
+	}
+
+	return ret
+}
+
+func (f *FixedLayout) View() string {
+	if f.components[0].size == 0 || f.components[1].size == 0 {
+		return "Terminal too small"
+	}
+
+	if f.horizontal {
+		return renderHorizontal(f.components[0].Model.View(), f.components[1].Model.View())
+	}
+
+	return strings.TrimRight(f.components[0].Model.View(), "\n") + "\n" + f.components[1].Model.View()
+}
+
+func renderHorizontal(left, right string) string {
+	left = strings.TrimRight(left, "\n")
+	right = strings.TrimRight(right, "\n")
+	bytes := len(left) + len(right)
+	leftLines := strings.Split(left, "\n")
+	rightLines := strings.Split(right, "\n")
+
+	var b strings.Builder
+	b.Grow(bytes)
+
+	for i, l, r := 0, len(leftLines), len(rightLines); i < max(l, r); i++ {
+		if i < l {
+			b.WriteString(leftLines[i])
+		} else {
+			b.WriteString(strings.Repeat(" ", len(leftLines[0])))
+		}
+		if i < r {
+			b.WriteString(rightLines[i])
+		} else {
+			b.WriteString(strings.Repeat(" ", len(rightLines[0])))
+		}
+	}
+	return b.String()
+}
---
+++ lg/fixed_render_test.go
@@ -0,0 +1,219 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package lg
+
+import (
+	"strings"
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/raohwork/huninn/tapioca"
+	"github.com/stretchr/testify/assert"
+)
+
+func splitFixedRows(output string, width, height int, horizontal bool) []string {
+	if horizontal {
+		rows := make([]string, 0, height)
+		for start := 0; start < len(output); start += width {
+			end := min(start+width, len(output))
+			rows = append(rows, output[start:end])
+		}
+		return rows
+	}
+
+	rows := strings.Split(output, "\n")
+	if len(rows) > height && rows[len(rows)-1] == "" {
+		rows = rows[:len(rows)-1]
+	}
+	return rows
+}
+
+func TestFixedLayout_View_NormalScenarios(t *testing.T) {
+	const (
+		termWidth  = 8
+		termHeight = 8
+		reserve    = 3
+	)
+
+	tests := []struct {
+		name         string
+		makeLayout   func(reserveComponent, restComponent tea.Model) *FixedLayout
+		horizontal   bool
+		expectedRows []string
+		reserveChar  string
+		restChar     string
+	}{
+		{
+			name: "left",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedLeftLayout(reserve, reserveComponent, restComponent)
+			},
+			horizontal: true,
+			expectedRows: []string{
+				"LLLRRRRR",
+				"LLLRRRRR",
+				"LLLRRRRR",
+				"LLLRRRRR",
+				"LLLRRRRR",
+				"LLLRRRRR",
+				"LLLRRRRR",
+				"LLLRRRRR",
+			},
+			reserveChar: "L",
+			restChar:    "R",
+		},
+		{
+			name: "right",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedRightLayout(reserve, restComponent, reserveComponent)
+			},
+			horizontal: true,
+			expectedRows: []string{
+				"LLLLLRRR",
+				"LLLLLRRR",
+				"LLLLLRRR",
+				"LLLLLRRR",
+				"LLLLLRRR",
+				"LLLLLRRR",
+				"LLLLLRRR",
+				"LLLLLRRR",
+			},
+			reserveChar: "R",
+			restChar:    "L",
+		},
+		{
+			name: "top",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedTopLayout(reserve, reserveComponent, restComponent)
+			},
+			horizontal: false,
+			expectedRows: []string{
+				"TTTTTTTT",
+				"TTTTTTTT",
+				"TTTTTTTT",
+				"BBBBBBBB",
+				"BBBBBBBB",
+				"BBBBBBBB",
+				"BBBBBBBB",
+				"BBBBBBBB",
+			},
+			reserveChar: "T",
+			restChar:    "B",
+		},
+		{
+			name: "bottom",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedBottomLayout(reserve, restComponent, reserveComponent)
+			},
+			horizontal: false,
+			expectedRows: []string{
+				"TTTTTTTT",
+				"TTTTTTTT",
+				"TTTTTTTT",
+				"TTTTTTTT",
+				"TTTTTTTT",
+				"BBBBBBBB",
+				"BBBBBBBB",
+				"BBBBBBBB",
+			},
+			reserveChar: "B",
+			restChar:    "T",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reserveWidth := reserve
+			restWidth := termWidth - reserve
+			reserveHeight := termHeight
+			restHeight := termHeight
+			if !tt.horizontal {
+				reserveWidth = termWidth
+				restWidth = termWidth
+				reserveHeight = reserve
+				restHeight = termHeight - reserve
+			}
+
+			reserveMock := &MockRenderComponent{}
+			reserveMock.On("Update", tapioca.ResizeMsg{Width: reserveWidth, Height: reserveHeight}).Return(reserveMock, nil)
+			reserveMock.On("View").Return(createExactSizeString(strings.Repeat(tt.reserveChar, reserveWidth*reserveHeight), reserveWidth, reserveHeight))
+
+			restMock := &MockRenderComponent{}
+			restMock.On("Update", tapioca.ResizeMsg{Width: restWidth, Height: restHeight}).Return(restMock, nil)
+			restMock.On("View").Return(createExactSizeString(strings.Repeat(tt.restChar, restWidth*restHeight), restWidth, restHeight))
+
+			layout := tt.makeLayout(reserveMock, restMock)
+			layout.handleResize(termWidth, termHeight)
+
+			rows := splitFixedRows(layout.View(), termWidth, termHeight, tt.horizontal)
+			assert.Equal(t, termHeight, len(rows), "should produce %d rows", termHeight)
+			assert.Equal(t, tt.expectedRows, rows)
+		})
+	}
+}
+
+func TestFixedLayout_View_TerminalTooSmall(t *testing.T) {
+	const (
+		termWidth  = 5
+		termHeight = 5
+		reserve    = 5
+	)
+
+	tests := []struct {
+		name       string
+		makeLayout func(reserveComponent, restComponent tea.Model) *FixedLayout
+		horizontal bool
+	}{
+		{
+			name: "left",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedLeftLayout(reserve, reserveComponent, restComponent)
+			},
+			horizontal: true,
+		},
+		{
+			name: "right",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedRightLayout(reserve, restComponent, reserveComponent)
+			},
+			horizontal: true,
+		},
+		{
+			name: "top",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedTopLayout(reserve, reserveComponent, restComponent)
+			},
+			horizontal: false,
+		},
+		{
+			name: "bottom",
+			makeLayout: func(reserveComponent, restComponent tea.Model) *FixedLayout {
+				return FixedBottomLayout(reserve, restComponent, reserveComponent)
+			},
+			horizontal: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reserveWidth := reserve
+			reserveHeight := termHeight
+			if !tt.horizontal {
+				reserveWidth = termWidth
+				reserveHeight = reserve
+			}
+
+			reserveMock := &MockRenderComponent{}
+			reserveMock.On("Update", tapioca.ResizeMsg{Width: reserveWidth, Height: reserveHeight}).Return(reserveMock, nil)
+
+			restMock := &MockRenderComponent{}
+
+			layout := tt.makeLayout(reserveMock, restMock)
+			layout.handleResize(termWidth, termHeight)
+
+			assert.Equal(t, "Terminal too small", layout.View())
+		})
+	}
+}
