28db12b711edd57806b37ec1efcf9208d0bbd91b
Ronmi Ren <ronmi.ren@gmail.com>
2025-10-03 13:39:38 +0800

    add default flavor

---
+++ doc.go
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Package huninn provides tools to build fency CLI applications. It
+// uses the Bubble Tea framework under the hood.
+//
+// It focuses on information displaying, not user interaction. A
+// Huninn component is a tea.Model with extra restrictions:
+//
+//   - It accepts tapioca.ResizeMsg.
+//   - View() method output a string exactly fitting its size.
+//
+// For example, a Huninn component with size 3x2 (specified by
+// tapioca.ResizeMsg) must output exactly 3 columns and 2 rows of
+// characters, like "abc\nd  ". Tailing newlines are optional.
+//
+// I'm planning to provide some ready-to-use presets, but for now
+// only [Default] is available.
+package huninn
